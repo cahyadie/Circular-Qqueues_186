0f06ea32a5f4d2063d57ad1d77e8c57bddd599c1
cahyadie <cahyadiilham12@gmail.com>
2023-06-22 15:39:43 +0700

    fix void display

--- Circular Qqueues/Circular Qqueues.cpp
+++ Circular Qqueues/Circular Qqueues.cpp
@@ -58,5 +58,23 @@ public:
 			else
 				FRONT = FRONT + 1;
 		}
+
 	}
+	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+		cout << "\n Element in the queue are...\n";
+
+		
+
+		
+	}
+	
+
 }; 
