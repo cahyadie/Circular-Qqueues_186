f09cb2f242ab4fa532dd92916f785325fb9b146f
cahyadie <cahyadiilham12@gmail.com>
2023-06-22 15:43:17 +0700

    void display and change position

--- Circular Qqueues/Circular Qqueues.cpp
+++ Circular Qqueues/Circular Qqueues.cpp
@@ -71,10 +71,15 @@ public:
 		}
 		cout << "\n Element in the queue are...\n";
 
-		
-
-		
+		//jika FRONT_position <= REAR_position, literasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "";
+				FRONT_position;
+			}
+			cout << endl;
+		}
 	}
-	
-
 }; 
+
+
