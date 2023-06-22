785861cda31c89fe3476c19d4bd735c192a461ec
cahyadie <cahyadiilham12@gmail.com>
2023-06-22 16:04:52 +0700

    fix display

--- Circular Qqueues/Circular Qqueues.cpp
+++ Circular Qqueues/Circular Qqueues.cpp
@@ -75,7 +75,14 @@ public:
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position <= REAR_position) {
 				cout << queue_array[FRONT_position] << "";
-				FRONT_position;
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			while (FRONT_position <= -1) {
+				cout << queue_array[FRONT_position] << "";
+				FRONT_position++;
 			}
 			cout << endl;
 		}
