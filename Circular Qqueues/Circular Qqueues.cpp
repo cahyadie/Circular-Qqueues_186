46fdc07175b577220f2297d6dacda1cc3d102440
cahyadie <cahyadiilham12@gmail.com>
2023-06-22 15:03:47 +0700

    untuk mengecek apakah antrian kosong

--- Circular Qqueues/Circular Qqueues.cpp
+++ Circular Qqueues/Circular Qqueues.cpp
@@ -17,10 +17,16 @@ public:
 		cin >> num;
 		cout << endl;
 
-		// cek apakah antrian penuh
+		//cek apakah antrian penuh
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 			return;
+		}
+
+		//Cek apakah antrian kosong 
+		if (FRONT == -1){
+			FRONT = 0;
+			REAR = 0;
 		}
 	}
 }; 
